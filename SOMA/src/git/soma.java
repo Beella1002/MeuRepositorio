2f223d243f3339dbc2647ad41562692daae81599
Bella <Bella@Isabella>
2014-10-14 15:05:33 -0300

    Adicionando o projeto

---
+++ SOMA/src/git/soma.java
@@ -0,0 +1,27 @@
+package git;
+
+import java.util.Scanner;
+
+public class soma {
+	
+	public static void main (String [] args){
+		
+		Scanner entrada = new Scanner (System.in);
+		
+		int num1;
+		int num2;
+		int soma;
+		
+		System.out.print("Digite o primeiro numero: ");
+		num1 = entrada.nextInt();
+		
+		System.out.print("Digite o segundo numero: ");
+		num2 = entrada.nextInt();
+		
+		soma = num1 + num2;
+		
+		System.out.println("Soma igual a " + soma);
+		
+	}
+
+}
